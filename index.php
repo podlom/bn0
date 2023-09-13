47e2bb06e10a1d6123e75e870e69ef42b5c36009
Taras Shkodenko <podlom@users.noreply.github.com>
2023-09-13 14:52:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
  * User: Тарас
  * Date: 20.12.2016
  * Time: 15:23
+ *
+ * Updated: 2023-09-13 14:52
  */
 
 require_once 'banner.class.php';
@@ -12,6 +14,9 @@ $lang = 'ru';
 if (isset($_GET['lang'])) {
     $lang = $_GET['lang'];
 }
-$bn = new app\banner($lang);
+if (isset($_GET['num'])) {
+    $num = $_GET['num'];
+}
+$bn = new app\banner($lang, $num);
 $bn->getHeaders();
 echo $bn->getBannerHtml();
