a5a0b4cb00df85023ce6a2052f964875e610511a
Taras Shkodenko <taras.shkodenko@gmail.com>
2025-06-14 17:24:45 +0300

    Log banner clicks

--- banner.class.php
+++ banner.class.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Тарас
  * Date: 20.12.2016
  * Time: 15:24
- * Updated: 2025-03-14 23:41
+ * Updated: 2025-06-14 17:23
  */
 
 namespace app;
@@ -240,6 +241,34 @@ class banner
             return $bannerHtml;
         }
 
+        // Витягуємо URL з тега <a href="...">
+        if (preg_match('/<a\s[^>]*href=[\'"]([^\'"]+)[\'"]/i', $bannerHtml, $matches91)) {
+            $this->logClick($matches91[1]);
+        }
+
         return $bannerHtml;
+    }
+
+    private function logClick($url)
+    {
+        $payload = [
+            'key' => '***REMOVED***', // <-- має відповідати Yii2 конфігурації
+            'url' => $url,
+            'ip' => $_SERVER['REMOTE_ADDR'] ?? null,
+            // 'country' => null,
+            // 'city' => null,
+            // 'isp' => null,
+            'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? null,
+            'network' => self::REF_NAME,
+            'lang' => $this->lang,
+        ];
+
+        $ch = curl_init('https://go.shkodenko.com/site/log-banner-click');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1); // не чекай довго
+        curl_exec($ch);
+        curl_close($ch);
     }
 }
